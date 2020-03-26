be87dd5efab4f6a19df9739b72efe134c9073468
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-03-26 17:20:27 -0400

    basically

---
+++ reactportfolio/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="text-muted">
+            <div class="container">
+                <p class="float-right">
+                    <a href="#">Back to top</a>
+                </p>
+
+                <p><a href="#">Visit the homepage</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- reactportfolio/src/components/Header.js
+++ reactportfolio/src/components/Header.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-export default class Header extends Component {
-    render() {
-        return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand bg-info" href="#">Zhaoyang Xia</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+import React from "react";
+import contactpage from "../pages/contact"
+function Header() {
 
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./index.html">About <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./portfolio.html">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./\contact.html" tabindex="-1" aria-disabled="true">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand bg-info" href="#">Zhaoyang Xia</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" >About <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href={{ contactpage }} tabindex="-1" aria-disabled="true">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+    
 }
 export default Header;
