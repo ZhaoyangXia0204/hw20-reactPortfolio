6c638786525ef7d0b4dfe5d62c09b9a6ddb11927
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-03-27 14:43:08 -0400

    Updates

---
+++ precache-manifest.330091b973f65bfd8231b77c712c0b12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d553d314ee0778271ce5aa9ced258fc",
+    "url": "/hw20-reactPortfolio/index.html"
+  },
+  {
+    "revision": "19639a3d98027a5deb65",
+    "url": "/hw20-reactPortfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a471200df44289767650",
+    "url": "/hw20-reactPortfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19639a3d98027a5deb65",
+    "url": "/hw20-reactPortfolio/static/js/2.6b8afcdf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hw20-reactPortfolio/static/js/2.6b8afcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a471200df44289767650",
+    "url": "/hw20-reactPortfolio/static/js/main.515dfd95.chunk.js"
+  },
+  {
+    "revision": "e217b7b85ac61b5638df",
+    "url": "/hw20-reactPortfolio/static/js/runtime-main.33665b8c.js"
+  },
+  {
+    "revision": "2afc41b408c1ef735c95db1952c325e1",
+    "url": "/hw20-reactPortfolio/static/media/zhao.2afc41b4.pdf"
+  }
+]);
